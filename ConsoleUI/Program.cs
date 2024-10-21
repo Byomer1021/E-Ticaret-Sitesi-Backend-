ad88070924561c6ab4d175faf6f5b2e13f42b4a4
ÖMER CAN <MER CAN@DESKTOP-T72PNN7>
2024-10-21 23:58:57 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+
+//SOLID
+//Open Closed Principle mevcut koda dokunmadan yeni bir özellik eklemek
+ProductManager productManager=new ProductManager(new EfProductDal());
+
+foreach (var product in productManager.GetByUnitPrice(40,100))
+{
+    Console.WriteLine(product.ProductName);
+}
